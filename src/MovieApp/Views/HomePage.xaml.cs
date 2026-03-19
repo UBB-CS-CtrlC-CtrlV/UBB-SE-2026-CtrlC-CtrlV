0070fb2c0d59305202cf0a712c2858954ee03cde
Andreea-Maria Buruiana <andreea.maria.buruiana@stud.ubbcluj.ro>
2026-03-19 11:56:20 +0200

    feat(KAN-175): implement initial navigation shell

---
+++ src/MovieApp/Views/HomePage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace MovieApp.Views;
+
+public sealed partial class HomePage : Page
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/MovieApp/Views/MyEventsPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace MovieApp.Views;
+
+public sealed partial class MyEventsPage : Page
+{
+    public MyEventsPage()
+    {
+        InitializeComponent();
+    }
+}
