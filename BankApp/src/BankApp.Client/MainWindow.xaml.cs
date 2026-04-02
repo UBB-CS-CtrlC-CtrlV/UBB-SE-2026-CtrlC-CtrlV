451405375c1acda043e9484e5dd45c6e7d41f01d
Rares Bogdan <bogdan.andrei.rares@gmail.com>
2026-04-02 14:03:32 -0700

    chore(BANK-670): cleanup MainWindow code

--- BankApp/src/BankApp.Client/MainWindow.xaml.cs
+++ BankApp/src/BankApp.Client/MainWindow.xaml.cs
@@ -1,17 +1,26 @@
-using Microsoft.UI.Xaml;
+// <copyright file="MainWindow.xaml.cs" company="CtrlC CtrlV">
+// Copyright (c) CtrlC CtrlV. All rights reserved.
+// </copyright>
+
 using BankApp.Client.Views;
+using Microsoft.UI.Xaml;
 
-namespace BankApp.Client
+namespace BankApp.Client;
+
+/// <summary>
+/// Hosts the application's root frame and initializes the first navigated view.
+/// </summary>
+public sealed partial class MainWindow : Window
 {
-    public sealed partial class MainWindow : Window
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-            App.NavigationService.SetFrame(RootFrame);
+        this.InitializeComponent();
+        App.NavigationService.SetFrame(this.RootFrame);
 
-            // Start on the login page
-            App.NavigationService.NavigateTo<LoginView>();
-        }
+        // Start on the login page
+        App.NavigationService.NavigateTo<LoginView>();
     }
 }
