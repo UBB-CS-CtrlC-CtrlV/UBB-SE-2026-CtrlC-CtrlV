cebad6957c4cdc2e2c736b027cfea3b89a29c668
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-04-05 14:22:27 +0300

    feat: add XML documentation to AuthController and extract VerifyTokenDto to separate file

---
+++ BankApp/src/BankApp.Server/Controllers/VerifyTokenDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BankApp.Server.Controllers
+{
+    /// <summary>
+    /// Data transfer object used for reset token verification requests.
+    /// </summary>
+    public class VerifyTokenDto
+    {
+        /// <summary>
+        /// Gets or sets the reset token to be verified.
+        /// </summary>
+        public string Token { get; set; } = string.Empty;
+    }
+}
