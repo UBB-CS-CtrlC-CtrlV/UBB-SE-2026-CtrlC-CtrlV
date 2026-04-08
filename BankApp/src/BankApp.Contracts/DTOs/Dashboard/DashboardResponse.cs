e704ef6c4b4cbe6358084ef878e5aecf8786dcec
Rares Bogdan <bogdan.andrei.rares@gmail.com>
2026-04-08 13:08:36 -0700

    fix(BANK-698): solve style-cop errors

--- BankApp/src/BankApp.Contracts/DTOs/Dashboard/DashboardResponse.cs
+++ BankApp/src/BankApp.Contracts/DTOs/Dashboard/DashboardResponse.cs
@@ -17,12 +17,12 @@ public class DashboardResponse
     /// <summary>
     /// Gets or sets the list of cards belonging to the user.
     /// </summary>
-    public List<CardDto> Cards { get; set; } = [];
+    public List<CardDto> Cards { get; set; } = new List<CardDto>();
 
     /// <summary>
     /// Gets or sets the list of recent transactions.
     /// </summary>
-    public List<TransactionDto> RecentTransactions { get; set; } = [];
+    public List<TransactionDto> RecentTransactions { get; set; } = new List<TransactionDto>();
 
     /// <summary>
     /// Gets or sets the count of unread notifications.
