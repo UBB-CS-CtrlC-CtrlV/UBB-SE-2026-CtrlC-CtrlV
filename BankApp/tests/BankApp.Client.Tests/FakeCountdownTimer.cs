b593cbca5115de8a439b7ffee7722b2cf7b8bfff
Alexandra-Maria-Chicus <alexandra.chicus@stud.ubbcluj.ro>
2026-04-16 21:16:38 +0300

    feat/test: add fake countdown timer

---
+++ BankApp/tests/BankApp.Client.Tests/FakeCountdownTimer.cs
@@ -0,0 +1,13 @@
+﻿using BankApp.Client.Utilities;
+
+namespace BankApp.Client.Tests;
+
+internal class FakeCountdownTimer : ICountdownTimer
+{
+    public event EventHandler? Tick;
+    public bool IsRunning { get; private set; }
+
+    public void Start() => this.IsRunning = true;
+    public void Stop() => this.IsRunning = false;
+    public void FireTick() => this.Tick?.Invoke(this, EventArgs.Empty);
+}
